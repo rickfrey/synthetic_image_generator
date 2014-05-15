ee938992982fc7f76798faf93c8f0a563450f763
imesadmin <imesadmin@imesros14.(none)>
2014-05-15 13:07:17 +0200

    comments

--- src/synthetic_image_generator.cpp
+++ src/synthetic_image_generator.cpp
@@ -31,31 +31,34 @@ int main ( int argc, char *argv[] )
   mapper->SetInputConnection(reader->GetOutputPort());
  
   vtkSmartPointer<vtkActor> actor =
-    vtkSmartPointer<vtkActor>::New();
+    vtkSmartPointer<vtkActor>::New();   // vtkActor is used to represent an entity (Einheit) in a rendering scene
+                                        // Verbindung zu mapper (Geometrische Definition), actor hat infos über Abstand etc. (z.B. auch Textur)
   actor->SetMapper(mapper);
 
   //TEST!!!///////////////////////////
-  vtkSmartPointer<vtkCamera> camera = vtkSmartPointer<vtkCamera>::New();
+  vtkSmartPointer<vtkCamera> camera = vtkSmartPointer<vtkCamera>::New(); // vtkCamera is a virtual camera for 3D rendering
   camera->SetPosition(0,0,10);
   camera->SetFocalPoint(0,0,0);
   ///////////////////////////////////
 
   vtkSmartPointer<vtkRenderer> renderer =
-    vtkSmartPointer<vtkRenderer>::New();
+    vtkSmartPointer<vtkRenderer>::New();    // A renderer is an object that controls the rendering process for objects. Rendering
+                                            // is the process of converting geometry, a specification for lights, and
+                                            // a camera view into an image
 
   //TEST!!!//////////////////////////
   renderer->SetActiveCamera(camera);
   ///////////////////////////////////
 
   vtkSmartPointer<vtkRenderWindow> renderWindow =
-    vtkSmartPointer<vtkRenderWindow>::New();
+    vtkSmartPointer<vtkRenderWindow>::New(); // create a window for renderers to draw into
   renderWindow->AddRenderer(renderer);
   //TEST///
   //renderWindow->SetSize(600,400);
   /////////
 
   vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
-    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    vtkSmartPointer<vtkRenderWindowInteractor>::New(); // Ermöglichst Interaktion mit Maus/ Tastatur
   renderWindowInteractor->SetRenderWindow(renderWindow);
  
   renderer->AddActor(actor);
@@ -65,8 +68,10 @@ int main ( int argc, char *argv[] )
 
   //TEST!!!///////////////////
   vtkSmartPointer<vtkWindowToImageFilter> ImageFilter = vtkSmartPointer<vtkWindowToImageFilter>::New();
-  ImageFilter->SetInput(renderWindow);//renderer/renderWindow???
-  vtkSmartPointer<vtkPNGWriter> pngWriter = vtkSmartPointer<vtkPNGWriter>::New();
+  ImageFilter->SetInput(renderWindow);  // vtkWindowToImageFilter provides methods needed to read the data in
+                                        // a vtkWindow and use it as input to the imaging pipeline. This is
+                                        // useful for saving an image to a file for example
+  vtkSmartPointer<vtkPNGWriter> pngWriter = vtkSmartPointer<vtkPNGWriter>::New(); // Writes PNG files
   pngWriter->SetFileName("output.png");
   pngWriter->SetInput(ImageFilter->GetOutput());
   pngWriter->Write();
