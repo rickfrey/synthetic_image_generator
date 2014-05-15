e5f0ec806243c9ff6a1b782313729b46d48257bc
imesadmin <imesadmin@imesros14.(none)>
2014-05-15 12:42:26 +0200

    Kameraposition setzen, als png exportieren (noch schwarz)

--- src/synthetic_image_generator.cpp
+++ src/synthetic_image_generator.cpp
@@ -6,6 +6,9 @@
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkCamera.h>
+#include <vtkWindowToImageFilter.h>
+#include <vtkPNGWriter.h>
  
 int main ( int argc, char *argv[] )
 {
@@ -24,18 +27,33 @@ int main ( int argc, char *argv[] )
  
   // Visualize
   vtkSmartPointer<vtkPolyDataMapper> mapper =
-    vtkSmartPointer<vtkPolyDataMapper>::New();
+    vtkSmartPointer<vtkPolyDataMapper>::New();// vtkPolyDataMapper is a class that maps polygonal data (i.e., vtkPolyData)
   mapper->SetInputConnection(reader->GetOutputPort());
  
   vtkSmartPointer<vtkActor> actor =
     vtkSmartPointer<vtkActor>::New();
   actor->SetMapper(mapper);
- 
+
+  //TEST!!!///////////////////////////
+  vtkSmartPointer<vtkCamera> camera = vtkSmartPointer<vtkCamera>::New();
+  camera->SetPosition(0,0,10);
+  camera->SetFocalPoint(0,0,0);
+  ///////////////////////////////////
+
   vtkSmartPointer<vtkRenderer> renderer =
     vtkSmartPointer<vtkRenderer>::New();
+
+  //TEST!!!//////////////////////////
+  renderer->SetActiveCamera(camera);
+  ///////////////////////////////////
+
   vtkSmartPointer<vtkRenderWindow> renderWindow =
     vtkSmartPointer<vtkRenderWindow>::New();
   renderWindow->AddRenderer(renderer);
+  //TEST///
+  //renderWindow->SetSize(600,400);
+  /////////
+
   vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
     vtkSmartPointer<vtkRenderWindowInteractor>::New();
   renderWindowInteractor->SetRenderWindow(renderWindow);
@@ -44,6 +62,16 @@ int main ( int argc, char *argv[] )
   renderer->SetBackground(.3, .6, .3); // Background color green
  
   renderWindow->Render();
+
+  //TEST!!!///////////////////
+  vtkSmartPointer<vtkWindowToImageFilter> ImageFilter = vtkSmartPointer<vtkWindowToImageFilter>::New();
+  ImageFilter->SetInput(renderWindow);//renderer/renderWindow???
+  vtkSmartPointer<vtkPNGWriter> pngWriter = vtkSmartPointer<vtkPNGWriter>::New();
+  pngWriter->SetFileName("output.png");
+  pngWriter->SetInput(ImageFilter->GetOutput());
+  pngWriter->Write();
+  ////////////////////////////
+
   renderWindowInteractor->Start();
 
  
