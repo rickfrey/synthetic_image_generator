aab5af22e40986bba90b9f4f26839fbe504fb5f9
sysadm <sysadm@imesbv01.(none)>
2014-07-28 16:26:45 +0200

    added Trackbar

--- src/handy_linienextraktor.cpp
+++ src/handy_linienextraktor.cpp
@@ -20,21 +20,23 @@ int main( int argc, char** argv )
         return -1;
     }
 
-    int threshold1= 60;
+    int threshold1 = 60;
+    int threshold2 = 40;
+    int threshold3 = 3;
     namedWindow( "source",WINDOW_NORMAL);
     namedWindow( "detected lines",WINDOW_NORMAL);
     namedWindow("Binär",WINDOW_NORMAL);
     cv::createTrackbar("Canny1","Binär",&threshold1,100,NULL);
-
+    cv::createTrackbar("Canny2","Binär",&threshold2,100,NULL);
         std::vector<Vec4i> lines;
     char c;
 
-    while(c!= 'q' || c!='s'){
-        c = getchar();
-        cv::Canny(Handybild,dst,threshold1,80,3);
+    while(c != 'q' && c != 's'){
+        c = (char)waitKey(2);
+        cv::Canny(Handybild,dst,threshold1,threshold2,3);
         cv::cvtColor(dst,cdst,CV_GRAY2BGR);
 
-
+        cout<<c<<endl;
 
 
         HoughLinesP(dst, lines, 1, CV_PI/360, 90, 30, 50 );
@@ -44,6 +46,10 @@ int main( int argc, char** argv )
 //            Vec4i l = lines[i];
             Vec4i l1 = lines[i];
             line( cdst, Point(l1[0], l1[1]), Point(l1[2], l1[3]), Scalar(0,0,255), 1, CV_AA);
+
+            Point p1;
+            //p1.x=
+            //circle(cdst,)
 //            //Eigenschaften berechnen:
 //            Mittelpunkt.x=cvRound((l[0]+l[2])/2);//Berechnung x-Koordinate des Mittelpunkts
 //            Mittelpunkt.y=cvRound((l[1]+l[3])/2);//  ""       y-koordinate
@@ -58,13 +64,11 @@ int main( int argc, char** argv )
 //            myfile<<"Mittelpunkt "<<i<<": "<<Mittelpunkt.x<<", "<<Mittelpunkt.y<<"; Theta: " << Theta << "; Länge: " << laenge << ";" <<std::endl;
         }
 
-        std::cout<<"Dimension von lines: "<<lines.size()<<std::endl;
+       // std::cout<<"Dimension von lines: "<<lines.size()<<std::endl;
 
 
         imshow("source", Handybild);
-
         imshow("detected lines", cdst);
-
         imshow("Binär",dst);
     }
 
@@ -85,13 +89,14 @@ int main( int argc, char** argv )
             cv::circle(cdst,Mittelpunkt,5,Scalar(0,255,0),2,CV_AA);
             Gegenkathete=((l[1]-l[3]));
             Ankathete=((l[2]-l[0]));
-            std::cout<<l[0]<<","<<l[1]<<","<<l[2]<<","<<l[3]<<std::endl;
+            std::cout << l[0] <<","<< l[1 ]<< "," << l[2] << "," << l[3] << std::endl;
             Theta=atan(Gegenkathete/Ankathete)*360/(2*CV_PI);
             //Theta=(CV_PI/2-Theta)*360/(2*CV_PI);
             laenge=cvRound(sqrt(Ankathete*Ankathete+Gegenkathete*Gegenkathete));
-
-            myfile<<"Mittelpunkt "<<i<<": "<<Mittelpunkt.x<<", "<<Mittelpunkt.y<<"; Theta: " << Theta << "; Länge: " << laenge << ";" <<std::endl;
+            // Mittelpunkt x, Mittelpunkt y, Theta, Länge
+            myfile << Mittelpunkt.x << " " << Mittelpunkt.y << " " << Theta << " " << laenge << std::endl;
         }
+        cout<<"Bildgröße="<< Handybild.rows << "x" << Handybild.cols <<endl;
         myfile.close();
     }
 
